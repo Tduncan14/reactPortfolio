b00cf164cc019edb88f1843b5d83e79bea6705ed
unknown <nbatre1@yahoo.com>
2019-02-07 02:31:14 -0600

    keep moving forward always

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,18 +1,41 @@
 import React,{Component} from 'react';
-
 class Jokes extends Component{
 
-    state = {joke:{}};
+    state = {joke:{} ,jokes:[] };
 
     componentDidMount(){
         fetch('https://official-joke-api.appspot.com/random_joke')
         .then((response) =>response.json())
-        .then( json => console.log('json',json))  
+        .then( (json) =>this.setState({joke:json}))  
+ }
+
+ fetchJokes=()=>{ 
+     fetch('https://official-joke-api.appspot.com/random_ten')
+     .then(response => response.json())
+     .then(json => this.setState({jokes: json}));
+
  }
 
  render(){
+    
+    const{setup,punchline} = this.state.joke;
+
      return(
-         <div></div>
+         <div>
+             <h2>Section Joke</h2>
+             <p>{setup}<em>{punchline}</em></p>
+             <hr /> 
+             <h3>Want more jokes</h3>
+             <button onClick={this.fetchJokes}> Press the Button</button>
+              {
+                this.state.jokes.map(joke =>{
+                   const{ id,setup,punchline} = joke
+
+                   return <p key={id}>{setup} <em>{punchline}</em></p>
+
+                })
+              }
+         </div>
      )
  }
 }
