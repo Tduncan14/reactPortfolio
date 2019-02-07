6b3d88c2a1db503ed2aa04b0c726ee6533d1d4e1
unknown <nbatre1@yahoo.com>
2019-02-07 11:49:23 -0600

    keep moving forward

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import{Router,Switch,Route} from'react-router-dom';
 import App from './components/App';
+import createBrowserHistory from 'history/createBrowserHistory';
 import Jokes from './components/Jokes';
 import index from './index.css';
 
